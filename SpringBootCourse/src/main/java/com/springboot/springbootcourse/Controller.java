f1637784986e9fb523d3e6ed160b2d518d66acec
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-08-09 13:06:58 +0200

    implemet method hello which return string

--- SpringBootCourse/src/main/java/com/springboot/springbootcourse/Controller.java
+++ SpringBootCourse/src/main/java/com/springboot/springbootcourse/Controller.java
@@ -1,4 +1,16 @@
 package com.springboot.springbootcourse;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value ="/")
 public class Controller {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+
+    public String hello(){
+        return "Hello End User";
+    }
 }
