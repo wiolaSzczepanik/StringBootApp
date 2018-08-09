f12e2825a01cfd47b463b547e792ea8b5cf45125
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-08-09 12:45:21 +0200

    add folder with Spring Boot from start.spring.io

---
+++ SpringBootCourse/src/main/java/com/springboot/springbootcourse/SpringBootCourseApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.springbootcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCourseApplication.class, args);
+	}
+}
