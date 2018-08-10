db0e86cca58d47193e2adf10db40ea5c6c472d6e
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-08-10 16:58:12 +0200

    add class fields to User class and add dependency to maven with lombok

--- SpringBootCourse/src/main/java/com/springboot/springbootcourse/User.java
+++ SpringBootCourse/src/main/java/com/springboot/springbootcourse/User.java
@@ -1,4 +1,14 @@
 package com.springboot.springbootcourse;
 
+import lombok.Data;
+
+@Data
 public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean author;
+    private boolean admin;
 }
